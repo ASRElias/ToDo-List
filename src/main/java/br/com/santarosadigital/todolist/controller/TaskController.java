418780f1ede3aa783cc327bd79c4588c60cb7a06
Elias Amigo <elias.rosa@quicard.com.br>
2021-10-18 06:54:40 -0300

    Ajustes para testes

--- src/main/java/br/com/santarosadigital/todolist/controller/TaskController.java
+++ src/main/java/br/com/santarosadigital/todolist/controller/TaskController.java
@@ -6,7 +6,6 @@ import br.com.santarosadigital.todolist.repository.TaskRepository;
 import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/br/com/santarosadigital/todolist/controller/UserController.java
+++ src/main/java/br/com/santarosadigital/todolist/controller/UserController.java
@@ -1,15 +1,12 @@
 package br.com.santarosadigital.todolist.controller;
 
-import br.com.santarosadigital.todolist.model.Task;
 import br.com.santarosadigital.todolist.model.User;
 import br.com.santarosadigital.todolist.repository.UserRepository;
 import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
