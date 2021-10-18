1cad1da6d1f31bc35be3250a2f980ac36e65f133
Elias Amigo <elias.rosa@quicard.com.br>
2021-10-18 01:14:13 -0300

    Inclusão de testes unitários iniciais

--- src/test/java/br/com/santarosadigital/todolist/ApiToDoApplicationTests.java
+++ src/test/java/br/com/santarosadigital/todolist/ApiToDoApplicationTests.java
@@ -1,12 +1,7 @@
 package br.com.santarosadigital.todolist;
 
-import br.com.santarosadigital.todolist.controller.TaskController;
-import br.com.santarosadigital.todolist.enumeration.TaskStatusEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ApiToDoApplicationTests {
--- src/test/java/br/com/santarosadigital/todolist/controller/TaskControllerTest.java
+++ src/test/java/br/com/santarosadigital/todolist/controller/TaskControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.santarosadigital.todolist.controller;
 
+import br.com.santarosadigital.todolist.model.Task;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +11,11 @@ class TaskControllerTest {
 
     @Test
     public void shouldGetTaskList() {
-
+        TaskController taskController = new TaskController();
+        List<Task> tasks = taskController.findAll("XPTO");
+        List<Task> expectedResponse = Collections.emptyList();
+        assertNotNull(tasks);
+        assertNotEquals(tasks, expectedResponse, "It should be empty");
     }
 
 }
